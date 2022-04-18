35991112b45bc3783aaf3950bacf61808f59605d
theyseemebowlin <96645197+theyseemebowlin@users.noreply.github.com>
2022-04-18 15:54:40 -0500

    Update runsteam.sh
    hopefully this will run steam or at least help you in your quest to run steam on arm64

--- runsteam.sh
+++ runsteam.sh
@@ -1,1 +1,33 @@
+# Make sure you have the non-free repository (of whatever distro you have) in your /etc/apt/sources.list enabled
+sudo apt install libappindicator1:armhf libnm0:armhf libtcmalloc-minimal4:armhf
+sudo dpkg -add-architecture i386
+sudo apt install steam:i386
+export STEAMOS=1 STEAM_RUNTIME=1
+echo "Are you on chrome os?"
+echo "1. Yes"
+echo "2. No, other distro"
+
+read choicevar
+
+if [ $choicevar -eq 1]
+        then
+        chromeos
+elif [ $choicevar -eq 2]
+        then
+        otherdistro
+
+function chromeos {
+        steam
+        export BOX86_LD_LIBRARY_PATH=~/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib/i386-linux-gnu:~/.local/share/Steam/ubuntu12_32
+        cd ~/.local/share/Steam/ubuntu12_32
+        BOX86_LOG=1 box86 ./steam
+}
+
+function otherdistro {
+        BOX86_LOG=1 steam
+}
+
+        
+
+
 
